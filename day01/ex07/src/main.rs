4d61e5de3f4a52a08aaa20109e5e3ccb2201323f
Louis Moulin <loumouli@e3r2p23.clusters.42paris.fr>
2023-03-14 19:45:42 +0100

    WIP ex07

--- day01/ex07/src/main.rs
+++ day01/ex07/src/main.rs
@@ -1,11 +1,42 @@
 use ftkit::{ARGS, read_line};
 
+fn remove_space( paragraphes:&mut Vec<String>, pos:usize ) {
+	let mut temp_str = paragraphes[pos].clone();
+	let mut mod_space = 0;
+	for mut i in 0..temp_str.len() {
+		println!("i = {}", i );
+		if temp_str.chars().nth(i).unwrap() == ' ' {
+			let temp = i + 1;
+			loop
+			{
+				if temp_str.chars().nth(temp).unwrap() == ' ' {
+					temp_str.remove(temp);
+					i -= 1;
+				}
+				else {
+					break ;
+				}
+			}
+		}
+	}
+
+}
+
+fn parsing_input( input:&String ) -> Vec<String> {
+	let mut paragraphes:Vec<String> = input.split(&"\n\n".to_string()).map(str::to_string).collect();
+	let mut i:usize = 0;
+	for i in 0.. paragraphes.len() {
+		if paragraphes[i].contains(' ') {
+			remove_space(&mut paragraphes, i);
+		}
+	}
+	let mut result:Vec<String> = Vec::new();
+	result
+}
+
 fn main() {
-    let args:Vec<&str> = ARGS.into_iter().collect();
-    if args.len() != 2 {
-        panic!("Wrong nnbr of args");
-    }
-    println!("{:?}", args);
-    let user_input = read_line();
-    println!("input = {}", user_input);
+	if ARGS.len() != 2 { panic!("Wrong nbr of args"); }
+	let nbr_colums = &ARGS[1];
+	let user_input = read_line();
+	let parsed_input:Vec<String> = parsing_input(&user_input);
 }
