6238cd091d42f1a4dfcd06ae6300a87e16121821
loumouli <louis.moulin@outlook.fr>
2023-07-27 10:33:29 +0200

    update module 00 ex07

--- module-00/ex07/src/lib.rs
+++ module-00/ex07/src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{assert, assert_eq};
+
+pub fn strcmp(query: &[u8], pattern: &[u8]) -> bool {
+    false
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
